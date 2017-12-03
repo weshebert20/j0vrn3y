ab6b4fabfd49c9e8d6515a182a9365309ae41771
weshebert20 <weshebert20@gmail.com>
2017-12-02 21:57:53 -0700

    added third page of photo

--- js/main3.js
+++ js/main3.js
@@ -1,23 +1,50 @@
-var frameNumber = 0, // start video at frame 0
-    // lower numbers = faster playback
-    playbackConst = 600, 
-    // get page height from video duration
-    setHeight = document.getElementById("set-height"), 
-    // select video element         
-    vid = document.getElementById('v0'); 
-    // var vid = $('#v0')[0]; // jquery option
-
-// dynamically set the page height according to video length
-vid.addEventListener('loadedmetadata', function() {
-  setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
-});
+// select video element
+var vid = document.getElementById('v0');
+var time = $('#time');
+var scroll = $('#scroll');
+var windowheight = $(window).height()-20;
 
 
-// Use requestAnimationFrame for smooth playback
-function scrollPlay(){  
-  var frameNumber  = window.pageYOffset/playbackConst;
-  vid.currentTime  = frameNumber;
-  window.requestAnimationFrame(scrollPlay);
-}
+var scrollpos = window.pageYOffset/400;
+var targetscrollpos = scrollpos;
+var accel = 0;
 
-window.requestAnimationFrame(scrollPlay);
+
+// ---- Values you can tweak: ----
+var accelamount = 0.001; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
+var bounceamount = 0.003; //value from 0 to 1 for how much backlash back and forth you want in the easing. 0 = no bounce whatsoever, 1 = lots and lots of bounce
+
+// pause video on load
+vid.pause();
+ 
+window.onscroll = function(){
+    //Set the video position that we want to end up at:
+    targetscrollpos = window.pageYOffset/200;
+  
+    //move the red dot to a position across the side of the screen
+    //that indicates how far we've scrolled.
+    scroll.css('top', 10+(window.pageYOffset/13500*windowheight));
+};
+
+
+setInterval(function(){  
+        
+      //Accelerate towards the target:
+      accel += (targetscrollpos - scrollpos)*accelamount;
+      
+      //clamp the acceleration so that it doesnt go too fast
+      if (accel > 1) accel = 1;
+      if (accel < -1) accel = -1;
+  
+      //move the video scroll position according to the acceleration and how much bouncing you selected:
+      scrollpos = (scrollpos + accel) * (bounceamount) + (targetscrollpos * (1-bounceamount));
+  
+      //move the blue dot to a position across the side of the screen
+      //that indicates where the current video scroll pos is.  
+      time.css('top', 10+(scrollpos/13500*400*windowheight));
+  
+      //update video playback
+      vid.currentTime = scrollpos;
+      vid.pause();
+    
+}, 40);
